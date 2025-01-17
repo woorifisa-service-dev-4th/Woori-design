952cff52098240038512c4bb8f0a1ed952112a72
1223v <1223v@naver.com>
2025-01-17 09:08:48 +0900

    Fix: css 버그 수정

--- src/lib/Index.jsx
+++ src/lib/Index.jsx
@@ -1,2 +1,3 @@
 export { default as WooriFloatButton } from "./floatbutton/WooriFloatButton";
+import "./floatbutton/WooriFloatButton.css"; // CSS 파일 포함
 //export { default as WooriModal } from "./modals/WooriModal";

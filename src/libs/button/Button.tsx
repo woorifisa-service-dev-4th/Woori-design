6c1bcc18f51a2c7fa1698df422c0e87442bacd5a
seocylucky <seocylucky@github.com>
2025-01-24 10:24:41 +0900

    refactor: 불필요한 onKeydown 삭제

--- src/libs/button/Button.tsx
+++ src/libs/button/Button.tsx
@@ -6,20 +6,12 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ size = "xlarge", width, children, onClick, ...props }, ref) => {
     const sizeClassName = styles[`button--${size}`];
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
-      if (event.key === "Enter" || event.key === " ") {
-        event.preventDefault();
-        onClick?.(event as unknown as React.MouseEvent<HTMLButtonElement>);
-      }
-    };
-
     return (
       <button
         ref={ref}
         className={`${styles.button} ${sizeClassName}`}
         disabled={props.disabled}
         style={width ? { width } : undefined}
-        onKeyDown={handleKeyDown}
         onClick={onClick}
         {...props}
       >

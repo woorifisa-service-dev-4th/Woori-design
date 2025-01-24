ca8daa707023eba20c5fbcfd44bcfd8d359390e3
seocylucky <seocylucky@github.com>
2025-01-24 11:16:23 +0900

    feat: tsconfig 설정 파일 작업

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // 플러그인 추가
+    cssInjectedByJsPlugin(),
+  ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/lib/Index.jsx"),
+      entry: path.resolve(__dirname, "src/index.ts"),
       name: "woori-design",
       fileName: (format) => `index.${format}.js`,
     },
@@ -19,6 +24,8 @@ export default defineConfig({
         },
       },
     },
-    cssCodeSplit: true, // CSS를 별도의 파일로 분리
+    // cssCodeSplit을 true로 하거나 false로 하거나
+    // cssInjectedByJsPlugin이 우선 적용되어 별도 파일이 안 나옵니다.
+    cssCodeSplit: true,
   },
 });
